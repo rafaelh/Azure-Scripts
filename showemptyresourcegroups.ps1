d190b5a32505ffe345ba5ff71ae4038ef4ef0987
Rafe Hart <rafael.hart9@gmail.com>
2019-04-09 16:14:44 +1000

    Still trying to get it working

--- showemptyresourcegroups.ps1
+++ showemptyresourcegroups.ps1
@@ -3,13 +3,13 @@ Login-AzureRmAccount
 
 #Get list of Azure Subscription ID's
 echo "Getting Subscriptions"
-$Subscriptions = (get-AzureRmSubscription).ID
+$Subscriptions = Get-AzureRmSubscription
 
 #Loop through the subscriptions to find all empty Resource Groups and store them in $EmptyRGs
 ForEach ($Subscription in $Subscriptions) {
-    $SubscriptionName = (Get-AzureRmSubscription).SubscriptionName
+    Select-AzureRmSubscription $Subscription | Out-Null
+    $SubscriptionName = (Select-AzureRmSubscription).Name
     echo "Processing $SubscriptionName"
-    Select-AzureRmSubscription -SubscriptionId $Subscription | Out-Null
     $AllRGs = (Get-AzureRmResourceGroup).ResourceGroupName
     $UsedRGs = (Get-AzureRmResource | Group-Object ResourceGroupName).Name
     $EmptyRGs = $AllRGs | Where-Object {$_ -notin $UsedRGs}
@@ -19,11 +19,3 @@ ForEach ($EmptyRG in $EmptyRGs) {
     echo "Empty Resource Group: $EmptyRGs"
 }
 
-# Loop through the empty Resorce Groups asking if you would like to delete them. And then deletes them.
-# foreach ($EmptyRG in $EmptyRGs){
-# $Confirmation = Read-Host "Would you like to delete $EmptyRG '(Y)es' or '(N)o'"
-# IF ($Confirmation -eq "y" -or $Confirmation -eq "Yes"){
-# Write-Host "Deleting" $EmptyRG "Resource Group" 
-# Remove-AzureRmResourceGroup -Name $EmptyRG -Force
-# } 
-# }
